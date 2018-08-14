3f6435eae8d8e3bf50f74ece7778071b6379b7a5
Álvaro Marqués Ramos <marquesramosalvaro@icloud.com>
2018-08-14 16:29:46 +0200

    Design updated
    Submit button animation improved

--- script.js
+++ script.js
@@ -1,95 +1,10 @@
-(function(){
-  var r = /\d{3}/;
-  console.log(r); // /\d{3}/
-  console.log(r.test("hello")); //false
-  console.log(r.test(123)); // true
-  console.log(r.test("ABC123")); // true (bad)
-
-  var r = /^\d{3}$/;
-  console.log(r.test(123)); // true
-  console.log(r.test("ABC123")); // false (good)
-});
-
-(function(){
-  // .MATCH
-  var s = "unicorns and rainbows and Vegan Cupcakes";
-  var r = /unicorn/;
-  console.log(s.match(r)); // ["unicorn"]
-
-  var r = /\w+/;
-  console.log(s.match(r)); // ["unicorns"]
-
-  // Flags: g -> global; i -> case insensitive
-  var r = /\b[a-z]+\b/;
-  console.log(s.match(r)); // ["unicorns", "and", "rainbows",...]
-});
-
-(function (){
-  // .EXEC (same as .match, but whit global flag also return the groups)
-  var s = "Here is are two numbers 111-2222 and 444-5555";
-  var r = /(\d{3})[-.]\d{4}/g;
-
-  console.log(r.exec(s));
-  console.log(r.exec(s));
-});
-
-(function(){
-  // .PLIT
-  var s = "Hey, I am testind the .split method";
-  // var r = /[\s,]+/;
-  var r = /(\W)/; // split rememering the regrex
-  console.log(s.split(r));
-})();
-
-(function(){
-// .REPLACE
-  // var s = "unicorns and rainbows and Vegan Cupcakes";
-  // var r = /\b\w{6,}\b/g;
-  // console.log(s.replace(r, "kittens"));
-
-  // var r = /([aeiou])/g;
-  // console.log(s.replace(r, "$1$1"));
-
-/////////////////////////////
-
-  // var s = "unicorns and rainbows and Vegan Cupcakes";
-  // var r = /\b\w+\b/g;
-  //
-  // var result = s.replace(r, replacer);
-  // console.log(result);
-  //
-  // function replacer(match){
-  //   // console.log(match);
-  //   if (match.length == 8){
-  //     return match.toUpperCase();
-  //   } else {
-  //     return match;
-  //   }
-  //   return "blueberry";
-  // }
-
-////////////////////////////
-
-  var s = "My number is 999-3828, and 393-8333.";
-  var r = /(\d{3})-(\d{4})/g;
-
-  var result = s.replace(r, replacer);
-
-  function replacer(match, g1, g2){
-    console.log(match);
-    console.log(g1);
-    console.log(g2);
-  }
-
-})();
-
-
 var string  = document.getElementById("string");
 var regex   = document.getElementById("regex")
 var flag    = document.getElementById("flag");
 var btn     = document.getElementById("btn");
 var text    = document.getElementById("text");
-
+var burger  = document.getElementById("burger");
+var func = document.getElementById("functions")
 function doRegex(){
   var s = string.value;
   var f = new RegExp(regex.value, flag.value);
@@ -107,9 +22,19 @@ function doRegex(){
   }else{
     return text.value = "Not matches :(";
   }
-}
+};
 
-btn.addEventListener("click", doRegex);
+
+function burgerIt(){
+  burger.classList.toggle("active");
+  func.classList.toggle("active");
+  btn.classList.toggle("active");
+}
+// burger.addEventListener("click", burger);
+
+
+
+
 
 
 // Try:
